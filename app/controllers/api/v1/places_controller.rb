e378a74cdb93d3fd20579c3ab71669ba0d4e38d7
msaleh-313 <msalehbutt313@gmail.com>
2024-08-16 16:31:41 +0500

    Destoy-action-api added successfully

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,12 +1,9 @@
 class Api::V1::PlacesController<ApplicationController
-
   before_action :set_place,only: %i[show update destroy]
-
   def index
     @places=Place.all
     render json:@places
   end
-
   def create 
     @place=Place.new(place_params)
     if @place.save
@@ -15,13 +12,9 @@ class Api::V1::PlacesController<ApplicationController
       render json:@place.errors.full_messages ,status: :unprocessable_entity
     end
   end
-
-  
-
   def show
     render json:@place      
   end
-
   def update
     if @place.update(place_params)
       render json: @place
@@ -29,18 +22,17 @@ class Api::V1::PlacesController<ApplicationController
       render json:@place.errors.full_messages, status: :unprocessable_entity
     end    
   end
-
- 
-
+  def destroy
+    @place.destroy
+    render json:{status: "success" , message: "Place has been deleted successfully"}    
+  end
   private
-
     def set_place
       @place=Place.find(params[:id])
     end
     def place_params 
       params.require(:place).permit(:name,:description,:latitude,:longitude,:city,:state,:country,:image_url)
     end
-
 end  
 
 

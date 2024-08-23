9438f34171806ccdfdffbc6920b40dbccbe3b270
msaleh-313 <msalehbutt313@gmail.com>
2024-08-23 16:28:57 +0500

    Eager Loading is added in index action

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -2,8 +2,11 @@ class Api::V1::PlacesController<ApplicationController
   before_action :set_place,only: %i[show update destroy]
   def index
     @places=Place.all
-    render json:@places.as_json(include: {images: {except: %i[imageable_type imageable_id created_at updated_at]}})
-    
+    # render json:@places
+    render json:@places.as_json(include: {images: {only: %i[id url]}})
+    # render json:@places.as_json(include: :images)
+
+    # render json:@places.as_json(include: {images: {except: %i[imageable_type imageable_id created_at updated_at]}})
     #   yeh method b use kar skte hein agar data chota hay toh 
     # render json:@places.map{|place| place.as_json(include: {images: {only: %i[id url]}})}
   end
@@ -17,9 +20,8 @@ class Api::V1::PlacesController<ApplicationController
   end
   def show
     # render json:@place
-    # render json:@place.as_json(include: :images)
     render json:@place.as_json(include: {images: {only: %i[id url]}})
-
+    
   end
   def update
     if @place.update(place_params)
